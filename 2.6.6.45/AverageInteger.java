4c247c2ab7b3c6bb23383b50d44078772f300059
Mark Kenneth Garcia <markkennethsgarcia@gmail.com>
2025-01-17 01:50:02 -0600

    saecond file commit

---
+++ 2.6.6.45/AverageInteger.java
@@ -0,0 +1,35 @@
+public class AverageInteger {
+    public static void main(String[] args) {
+
+        /*
+         * Ensure that if the values of the items being used to calculate the average
+         * can be modified with minimal
+         * changes to your program code.
+         * The values being averaged must be declared as integers.
+         */
+
+        // List of numbers //
+        int firstNumber = 1;
+        int secondNumber = 7;
+        int thirdNumber = 9;
+        int fourthNumber = 34;
+
+        // Counter for the integer values
+        double counter = 4.0;
+
+        // Formula = Get the sum of all listed values and then Divide it on how many
+        // values are there //
+
+        int integerTotalSumOfValues = firstNumber + secondNumber + thirdNumber + fourthNumber;
+
+        double calculatedValues = integerTotalSumOfValues / counter;
+
+        // Print //
+
+        System.out.println("The average of the values " + firstNumber + ", " + secondNumber + ", " + thirdNumber
+                + " and " + fourthNumber + " is " + calculatedValues);
+
+        // expectd output : The average of the values 1, 7, 9 and 34 is 12.75.
+
+    }
+}
