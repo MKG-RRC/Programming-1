13b2c82fc836653f2cdd379c20dde667eeb1c4cb
Mark Kenneth Garcia <markkennethsgarcia@gmail.com>
2025-01-17 21:48:33 -0600

    third part

---
+++ 2.6.7.48/KilogramsToPounds.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ * Assignment 1
+ * Name: Mark Kenneth Garcia
+ * Program/course: Full Stack Web Development / Programmng 1
+ * Created: 2025-01-17
+ * Updated: 2025-01-17
+ */
+
+ public class KilogramsToPounds {
+    public static void main (String[] args) {
+        
+    }
+ }
